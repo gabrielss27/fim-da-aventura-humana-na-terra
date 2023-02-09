99f3ae3127f99d124cf25ba1e6eac09499d61c02
Henrique Santana <henriquessantana314@gmail.com>
2023-02-09 00:10:06 -0300

    Voting mock

---
+++ src/provider/VoteProvider.js
@@ -0,0 +1,33 @@
+export const nCardGroups = 3
+export const nCardsPerGroup = 7
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export class VoteProvider {
+  constructor() {
+    const cardsKeys = Array(nCardGroups)
+      .fill()
+      .flatMap((_, group) =>
+        Array(nCardsPerGroup)
+          .fill()
+          .map((_, card) => `${group}-${card}`)
+      )
+    this.mockedVotes = {}
+    cardsKeys.forEach(cardKey => {
+        const votes = Math.floor(Math.random() * 42)
+        this.mockedVotes[cardKey] = votes
+    })
+  }
+
+  async getVotes() {
+    return this.mockedVotes
+  }
+
+  async vote(cardKey) {
+    this.mockedVotes[cardKey]++
+    await sleep(1000)
+    return this.mockedVotes
+  }
+}
