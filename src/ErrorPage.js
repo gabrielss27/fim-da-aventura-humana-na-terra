cd2881d0eebbc6490fbbf6d5e1ee24156db3dbc9
Henrique Santana <henriquessantana314@gmail.com>
2023-02-09 00:09:54 -0300

    Error page

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,16 +1,26 @@
 import { useRouteError } from "react-router-dom"
+import { useLocale } from "./i18n"
 
 export const ErrorPage = () => {
   const error = useRouteError()
+  const text = useLocale()[1]
   console.error(error)
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
+    <div
+      id="error-page"
+      className="page centered-page"
+      style={{ backgroundColor: "var(--background)", padding: "10rem" }}
+    >
+      <h1 className="light-text centered-text" style={{ marginBottom: "1rem" }}>
+        {text.title.error}
+      </h1>
+      <p className="light-text centered-text" style={{ marginBottom: "1rem" }}>
+      {text.about.error}
       </p>
+      <code className="light-text centered-text">
+        {error.statusText || error.message}
+      </code>
     </div>
   )
 }
